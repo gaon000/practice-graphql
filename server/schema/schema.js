d5ac0bc54ece45ad7903855d83d40d80837c3ea2
gaon000 <gaon000@naver.com>
2021-09-05 00:59:14 +0900

    root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,28 @@
 const graphql = require('graphql');
 
-const { GraphQLObjecType, GraphQLString } = graphql;
+const { GraphQLObjecType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjecType({
   name: 'Book',
-  field: () => ({
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
-})
+});
+
+const RootQuery = new GraphQLObjecType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args){
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
