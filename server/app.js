2f11d51767fa8568f7e689d990ecd5bb79cd152a
gaon000 <gaon000@naver.com>
2021-09-10 22:28:46 +0900

    connect to mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://test:test123@cluster0.vdthl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+  console.log('connected to db');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
