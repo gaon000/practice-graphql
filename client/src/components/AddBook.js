5cb014f2bf69a8ac4ba6d3414ed6136bb637d782
gaon000 <gaon000@naver.com>
2021-09-25 04:03:26 +0900

    update component state

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,27 +1,48 @@
 import { useQuery, gql } from '@apollo/client';
 import { getAuthors } from './queries/queries';
+import React, {useState} from "react";
 
-function AddBook(){
+function AddBook(props){
+  const [state, setState] = useState({
+    name: '',
+    genre: '',
+    authorId: ''
+  }
+  );
   const { loading, error, data } = useQuery(getAuthors);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
-  console.log(data);
-  return (
-    <form id="add-book">
 
+  function submitForm(e){
+    e.preventDefault();
+    console.log(state);
+  }
+
+  return (
+    <form id="add-book" onSubmit={submitForm}>
+    
     <div className="field">
       <label>Book name:</label>
-      <input type="text"/>
+      <input type="text" onChange={(e) => setState(prevState => ({
+        ...prevState,
+        name: e.target.value
+      }))}/>
     </div>
     
     <div className="field">
       <label>Genre:</label>
-      <input type="text"/>
+      <input type="text" onChange={(e) => setState(prevState => ({
+        ...prevState,
+        genre:e.target.value
+      }))} />
     </div>
 
     <div className="field">
       <label>Author:</label>
-      <select>
+      <select onChange={(e) => setState(prevState => ({
+        ...prevState,
+        authorId:e.target.value
+      }))}>
         <option>Select Author</option>
         {data.authors.map(author => {
           return ( <option key={author.id} value={author.id}>{author.name}</option> )
